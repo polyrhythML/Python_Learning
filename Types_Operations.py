db4b9b864b76c8a1b189340ae54be9dd48ef49ed
Amit Singh Bhatti <amit02093@gmail.com>
2019-07-09 01:58:50 +0530

    Updated Types and Operations

--- Types_Operations.py
+++ Types_Operations.py
@@ -18,3 +18,29 @@ print(S[1:])        # outputs pam
 print(S[:-1])       # outputs spa
 print(S[:])         # outputs spam
 print(S+"xyz")      # outputs spamxyz
+print(S*4)          # outputs spamspamspamspam
+
+# We see that S uses + as well as * in the string operations which is also used with integers
+# This is called Polymorphism
+# The operation type depends upon the type of the object itself.
+
+"""
+String are immutable types in python means cannot be changed inplace
+after they are created or you can never overwrite the values of immutable objects.
+"""
+# Below operation cannot be performed
+#S[0] = "z"
+# error --> TypeError: 'str' object does not support item assignment
+
+# Type Specific Methods :
+print(S.find("pa"))             # outputs 1
+print(S.replace("a", "bit"))    # outputs spbitm
+
+line = "aas,ssdad,EHE,SDsd,dSD,SD,dSDWFGWETH"
+print(line.split(","))          # outputs : ['aas', 'ssdad', 'EHE', 'SDsd', 'dSD', 'SD', 'dSDWFGWETH']
+line = line + "sdsd   "
+print(line.rstrip())            # outputs aas,ssdad,EHE,SDsd,dSD,SD,dSDWFGWETHsdsd
+                                # removes the right side spaces
+
+
+
