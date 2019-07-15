2ef0c9cd8a77ddb78921cba84ab11928e7eabdd0
Amit Singh Bhatti <amit02093@gmail.com>
2019-07-15 22:36:07 +0530

    updated the strings.py script

--- Strings.py
+++ Strings.py
@@ -63,6 +63,151 @@ There are about 17 planes
 eg U+1D360, 1 is the code plane and D360 is the code for a particular value
 """
 
+# Byte objects - Sequences of bytes
+# String object - Sequence of characters
+# Bytes are machine readable
+
+# For \ character in the string use \\ or use r""
+print(r"\trippycode")           # outputs :\trippycode
+print("\\trippycode")           # outputs : \trippycode
+
+"""
+Note : A string cannot end with a single backlash because the backlash escapes the 
+following quote characters, to embed it in the string.
+
+"""
+#print(r"amitnssdinasdo\")           # SyntaxError: EOL while scanning string literal
+# or
+#print("amitnssdinasdo\")            # SyntaxError: EOL while scanning string literal
+# If you want to have a string \ at the end of a raw string, you can do the following
+a = r"amitsingh\\"
+b = a[:-1]
+print(b)
+
+# Iterating over a Sting
+
+string = "trippycode"
+
+for i in string :
+    print(i)
+
+# Outputs :
+# t
+# r
+# i
+# p
+# p
+# y
+# c
+# o
+# d
+# e
+
+print(i in string)  # memebership test   # Returns True
+print("am" in "amrickskdonamndan")       # Substring search     # Returns True
+
+# Indexing and slicing
+#  t   r  i  p  p  y  c  o  d  e
+#  0   1  2  3  4  5  6  7  8  9
+# -10 -9 -8 -7 -6 -5 -4 -3 -2 -1
+print(string[0])            # outputs : t
+print(string[1:3])          # outputs : ri
+print(string[:])            # outputs : trippycode
+print(string[-2:-1])        # outputs : d
+print(string[:-1])          # outputs : trippycod
+print(string[-3:])          # outputs : ode
+print(string[2:6:2])        # outputs : ipc
+print(string[1::3])         # outputs : rpo
+print(string[-6:-3:2])      # outputs : pc
+print(string[-6:3:-2])      # outputs : p
+print(string[2:-10:1])      # outputs :
+print(string[2:-10:-2])     # outputs : i
+
+# Binary value to Character conversion in python
+print(ord('s'))             # outputs : 115
+print(chr(115))             # outputs : s
+
+# Strings are immutable
+# inplace changes cannot be made
+# need to use string.replace method or we can concatenate the string
+
+
+"""
+NOTE : Every operation that yields a new string value , string methods generate new strings objects.
+If you want to retain those objects, you can assign them to variable names 
+Generating a new string object for each string, but this is not in efficient as python garbage collects
+unreferenced string objects. 
+"""
+
+### STRING METHODS
+
+# Methods that are attribute attached to the object that happens to reference to the callable functions
+# Always have an implied subject
+# Attribute fetch and Call  expression
+# Below example examples it better
+
+a = "trippycode"
+print(a.find("co"))            # Here find- is the attribute to fetch
+                                    # calling the find("co") is the callable expression
+
+# Also , its better to note that string.replace("co", "d") created a new string
+# Let's check
+b = a.replace("co", "d")
+print(b is a)              # False
+print(b == a)              # False
+
+"""
+Performance improvement with string objects : 
+If you want to get better performance over a string of larger size where you are performing operations over it,
+better to convert the string into a type which has inplace operations, because otherwise each string operation 
+leads to a new string object
+"""
+S = "sammy"
+L = list(S)     # list is mutable type , therefor inplace changes
+print(L)        # outputs : ["s", "a", "m", "m", "y"]
+
+# to get back the string
+back = "".join(L)       # Sammy is back
+print(back)             # outputs : sammy
+
+# Joining Substrings works faster compared to concatenating substrings individually
+
+# STRING FORMATTING
+
+print("%d %s %g you"%(1, "spam", 4.0))              # Outputs : 1 spam 4 you
+print("%d %s %s you"%(1, "spam", 4.0))              # Outputs : 1 spam 4.0 you
+
+# Basically, in the above expression every variabe is converted into a string
+
+# Some String formatting for floating point numbers
+x = 1.23452452495563
+print("%e | %f | %g"%(x, x, x))         # outputs : 1.234525e+00 | 1.234525 | 1.23452
+# %e - scientific notation
+# %f - floating point notation
+# %g - real number notation
+
+print("%.2f"%(x))                       # Outputs : 1.23 decimal places number
+
+# Dictionary based Formatting
+print("%(qty)d more %(food)s"%{"qty": 1, "food" : "spam"})     # outputs : 1 more spam
+
+# Some more string formattings
+
+#print("{0:.f}, {1:.2f}, {2:06.2f}".format(x, x, x))
+
+print("{0:X}, {1:o}, {2:b}".format(255, 255, 255))          # outputs : FF, 377, 11111111
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
