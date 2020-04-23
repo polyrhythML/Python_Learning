25002fc3fa0ccc40207c2717ec2c4a80fb2b92d2
Amit Singh Bhatti <amit.bhatti@inspirit-iot.com>
2020-04-23 12:19:06 +0530

    Started with exceptions today

---
+++ Exceptions/Exception_coding.py
@@ -0,0 +1,12 @@
+# Overview
+
+"""
+* Events which modify the flow of control through a program.
+* try/except : Catch and recover from the exception in python
+* try/finally : Perform a cleanup action, whether exception occurs or not.
+* assert : Conditional trigger an exception in your code.
+* raise : Trigger an exception manually in your code.
+
+Why use exceptions ?
+* If your function executing fails, exception routes the code run to an alternate path.
+"""
